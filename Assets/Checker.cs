ac1fcb3252183d5540f167fe7e4c1437a71db849
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-09-01 15:45:31 +0900

    ファイルを追加

---
+++ Assets/Checker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Checker : MonoBehaviour
+{
+    public DeleteObject target;
+    void OnDisable()
+    {
+        target.OnDestroyed.RemoveAllListeners();
+    }
+    void OnEnable()
+    {
+        target.OnDestroyed.AddListener(() => {
+            Debug.Log("targetがDestroyされました");
+            // ここに処理を追加
+        });
+    }
+}
---
+++ Assets/DeleteObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class DeleteObject : MonoBehaviour
+{
+    public UnityEvent OnDestroyed = new UnityEvent();
+    private void OnDestroy()
+    {
+        Debug.Log("Destroyed");
+        OnDestroyed.Invoke();
+    }
+}
